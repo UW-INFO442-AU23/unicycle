4fbfa126f466e1c50f28dafc8423d4d00cf20134
wwang25-2066240 <q1958764146@outlook.com>
2023-10-18 11:27:35 -0700

    Firebase Setup

--- src/index.js
+++ src/index.js
@@ -4,6 +4,23 @@ import './index.css';
 import App from './App';
 
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCTRy3I87K7ZWLNYqovYZY-zHo-iosUPqc",
+  authDomain: "unicycle-f1cf4.firebaseapp.com",
+  projectId: "unicycle-f1cf4",
+  storageBucket: "unicycle-f1cf4.appspot.com",
+  messagingSenderId: "258287418145",
+  appId: "1:258287418145:web:07dadf8de6db3c7d931c60"
+};
+const app = initializeApp(firebaseConfig);
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
