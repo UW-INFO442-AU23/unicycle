68f0c152abe0688727b24224d81643fdc1cd87ce
wwang25-2066240 <q1958764146@outlook.com>
2023-10-23 14:22:31 -0700

    add components

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import NavBarPC from './components/NavBar/NavBarPC.js';
+import NavBarMobile from './components/NavBar/NavBarMobile.js';
+import Home from './components/Home.js';
+import Footer from './components/Footer.js';
+import EventSubscriptionForm from './components/EventSubscriptionForm.js';
+import Error from './components/Error.js';
+import Filter from './components/WasteBank/Fiilter.js';
+import Search from './components/WasteBank/Search.js';
+import WasteCard from './components/WasteBank/WasteCard.js';
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+
+export default function App(props) {
+    return (
+      <div>
+        <Router>
+          <Routes>
+  
+          </Routes>
+        </Router> 
+      </div>
+    );
+  }
+  
